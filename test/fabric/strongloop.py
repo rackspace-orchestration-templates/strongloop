0f0a580f2a41d54e82f62a22a4a3bd69189b6cdc
Jason Boyles <jason@duncancreek.net>
2014-08-14 20:17:54 -0500

    add tests

---
+++ test/fabric/strongloop.py
@@ -0,0 +1,23 @@
+from fabric.api import env, run, task
+from envassert import detect, file, group, package, port, process, service, \
+    user
+
+
+@task
+def check():
+    env.platform_family = detect.detect()
+
+    assert file.exists('/home/strongloop/loopback-example-app/package.json'), \
+        '/home/strongloop/loopback-example-app/package.json does not exist'
+    assert file.exists('/etc/supervisor.d/strongloop.conf'), \
+        '/etc/supervisor.d/strongloop.conf does not exist'
+
+    assert port.is_listening(3000), 'port 3000/node is not listening'
+
+    assert user.exists('strongloop'), 'user strongbad does not exist'
+    assert group.is_exists('strongloop'), 'group strongloop does not exist'
+
+    assert process.is_up('supervisord'), 'process supervisord is not running'
+    assert process.is_up('node'), 'process node is not running'
+
+    assert service.is_enabled('supervisor'), 'service supervisor is not enabled'
